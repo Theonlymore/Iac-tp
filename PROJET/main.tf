3a841bb639fe8cf7cb07e5d39bcec608eba7bf0e
warzota <132451984+warzota@users.noreply.github.com>
2025-01-10 11:20:47 +0100

    main.tf

--- PROJET/main.tf
+++ PROJET/main.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  region = var.aws_region  # Utilise une variable pour la région AWS
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = var.ami_id             # Image AMI pour l’instance
+  instance_type = var.instance_type      # Type d'instance
+
+  tags = {
+    Name = "TestInstance"                # Nom de l'instance
+  }
+}
+
+
 git add .
 git commit -m "Ajout du workflow Terraform"
 git push origin main
