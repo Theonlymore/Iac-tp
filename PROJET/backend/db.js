d5120ac075365e85a345ff070021de78cf569ff3
Skabold <pierre.linz@orange.fr>
2025-01-10 10:09:24 +0100

    there

--- PROJET/backend/db.js
+++ PROJET/backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'rootpassword',
-    database: 'testdb',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'rootpassword',
+    database: process.env.DB_NAME || 'testdb',
 });
 
 db.connect((err) => {
